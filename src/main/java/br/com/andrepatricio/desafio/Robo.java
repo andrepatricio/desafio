186b0307869ea87b244677c39276e008f05a25b7
Andre Patricio <andrepatricio2@gmail.com>
2018-01-03 17:41:01 -0200

    Commit inicial

---
+++ src/main/java/br/com/andrepatricio/desafio/Robo.java
@@ -0,0 +1,29 @@
+package br.com.andrepatricio.desafio;
+
+public class Robo {
+	
+	private Integer positionX;
+	private Integer positionY;
+	private Character direction;
+	public Integer getPositionX() {
+		return positionX;
+	}
+	public void setPositionX(Integer positionX) {
+		this.positionX = positionX;
+	}
+	public Integer getPositionY() {
+		return positionY;
+	}
+	public void setPositionY(Integer positionY) {
+		this.positionY = positionY;
+	}
+	public Character getDirection() {
+		return direction;
+	}
+	public void setDirection(Character direction) {
+		this.direction = direction;
+	}
+	public void turn(Character direction) {
+		
+	}
+}
